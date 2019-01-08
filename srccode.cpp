ab6f00418100acc6d4c774caa1bb7906f95ecbaa
xmenji <35322102+xmenji@users.noreply.github.com>
2019-01-08 00:22:19 -0800

    Update srccode.cpp

--- srccode.cpp
+++ srccode.cpp
@@ -1,28 +1,141 @@
-#include <iostream>
+#include <iostream> 
+#include <iomanip>
 using namespace std;
 
 int main() {
-	int num1,
+
+	const int ADDITION = 1,
+		SUBTRACTION = 2,
+		MULTIPLICATION = 3,
+		DIVISION = 4,
+		USER_QUIT = 5;
+
+	int userChoice,
+		num1,
 		num2,
+		userAnswer,
 		result;
 
-	//Prompt user for two numbers
-	cout << "Enter two numbers to add together\n";
-	cout << "First number: ";
-	//Get first number
-	cin >> num1;
-	cin.ignore();
-	//Get second number
-	cout << "Second number: ";
-	cin >> num2;
-	//Pause and wait for user to solve answer before pressing [Enter] to see the solution
-	cout << "\nPress [Enter] to see the answer.\n";
-	cout << num1 << " + " << num2;
-	cin.ignore();
-	cin.get();
-	//Calculate result and display answer to the user.
-	result = num1 + num2;
-	cout << "\nAnswer: " << result << endl;
+	do {
+		cout << setw(50) << "Select an option.\n";
+		cout << "1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Quit Program\n\n";
+		cout << "Enter choice: ";
+		cin >> userChoice;
+
+		switch (userChoice) {
+			case ADDITION: 
+				//Get first number
+				cout << "First number: ";
+				cin >> num1;
+				cin.ignore();
+				//Get second number
+				cout << "Second number: ";
+				cin >> num2;
+				//Calculate result 
+				result = num1 + num2;
+
+				cout << "\nInput answer and press [Enter] to check.\n";
+				cout << num1 << " + " << num2 << " = ";
+
+				//get user input
+				cin >> userAnswer;
+
+				//compare user answer to result
+				if (userAnswer == result)
+					cout << "\nCongratulations! The answer " << userAnswer << " is correct.\n\n";
+				else
+					cout << "\nIncorrect.\nAnswer: " << result << endl;
+				break;
+
+			case SUBTRACTION:
+				//Get first number
+				cout << "First number: ";
+				cin >> num1;
+				cin.ignore();
+				//Get second number
+				cout << "Second number: ";
+				cin >> num2;
+				//Calculate result
+				result = num1 - num2;
+
+				cout << "\nInput answer and press [Enter] to check.\n";
+				cout << num1 << " - " << num2 << " = ";
+
+				//get user input
+				cin >> userAnswer;
+
+				//compare user answer to result
+				if (userAnswer == result)
+					cout << "\nCongratulations! The answer " << userAnswer << " is correct.\n\n";
+				else
+					cout << "\nIncorrect.\nAnswer: " << result << endl;
+				break;
+
+			case MULTIPLICATION:
+				//Get first number
+				cout << "First number: ";
+				cin >> num1;
+				cin.ignore();
+				//Get second number
+				cout << "Second number: ";
+				cin >> num2;
+				//Calculate result
+				result = num1 * num2;
+
+				cout << "\nInput answer and press [Enter] to check.\n";
+				cout << num1 << " * " << num2 << " = ";
+
+				//get user input
+				cin >> userAnswer;
+
+				//compare user answer to result
+				if (userAnswer == result)
+					cout << "\nCongratulations! The answer " << userAnswer << " is correct.\n\n";
+				else
+					cout << "\nIncorrect.\nAnswer: " << result << endl;
+				break;
+
+			case DIVISION:
+				int remainder,
+					userRemainder;
+				//Get first number
+				cout << "First number: ";
+				cin >> num1;
+				cin.ignore();
+				//Get second number
+				cout << "Second number: ";
+				cin >> num2;
+				cin.ignore();
+				//Calculate result
+				result = num1 / num2;
+				remainder = num1 % num2;
+
+				cout << "How many times does "<< num2 << " go into " << num1 << "?\t";
+				cin >> userAnswer;
+				cin.ignore();
+				
+				cout << "How many Remainders are left over?\t";
+				cin >> userRemainder;
+				
+				//compare user answer to result
+				if (userAnswer == result && userRemainder == remainder) {
+					cout << "\nCongratulations! The answer " << userAnswer << " with a remainder of "
+						<< userRemainder << " is correct.\n\n";
+				}
+				else {
+					cout << "\nIncorrect.\nAnswer: " << result << " with a remainder of " << remainder << endl;
+				}
+				break;
+
+			case USER_QUIT:
+				cout << "\n\nProgram ending...\n";
+				break;
+
+			default: 
+				cout << "\n\nThe Valid choices are 1 - 5. Please select another option.\n";
+		}
+
+	} while (userChoice != USER_QUIT);
 
 	return 0;
 }
